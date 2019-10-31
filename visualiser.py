88666fb4a5ad45edadec6903ef28121e4896b8a0
Antioch <53316182+Antiochian@users.noreply.github.com>
2019-10-31 01:01:26 +0000

    Cleaned up some code, added more comments

--- visualiser.py
+++ visualiser.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*- python3
 """
-
 Sorting Visualiser
 Created on Sun Oct 27 14:45:40 2019
 
@@ -9,27 +8,23 @@ Created on Sun Oct 27 14:45:40 2019
 import pygame
 import random
 import numpy as np
-from bubblesort import *
-from quicksort import *
 
 #optional flags
 colorfade = True #makes the brightness of each bar proportional to its value
 uniformdist = True #makes the final result evenly-spaced bars
-algorithm = "quick" #also accepts "bubble"
+algorithm = "quick" #default starting algorithm
 
-#color settings
+#color data
 white = (255,255,255)
 red = (220, 50, 47) #m1 = red
 blue = (38, 139, 210) #m2 = blue
 green = (133,153,0)
 darkblue = (7,54,66)
 beige = (253,246,227)
-
 bgcolor = darkblue
 barcolor = green
 
-
-#parameters
+#display parameters
 Nx,Ny = (800,480)
 barwidthscale = 0.9
 defaultN = 200 #number of bars
@@ -39,6 +34,8 @@ animFPS = 10000
 menuFPS = 60
 
 #pygame setup
+#main animation is drawn on "surf" layer, with a semi-transparent "pausescreen" layer for
+#fade effects (introduced in the fadein function) and text layers overtop.
 pygame.init()
 window = pygame.display.set_mode( (Nx,Ny) )
 window.fill(bgcolor)
@@ -49,17 +46,11 @@ window.blit(surf,(0,0))
 clock = pygame.time.Clock() 
 
 ######### ALGORITHMS ############
-def bubblesort_recorder(array):
-    while True:
-        unsorted = True
-        for i in range(1,len(array)):
-            if array[i-1] > array[i]:
-                array[i-1],array[i] = array[i],array[i-1]
-                global record
-                record = np.append(record,[array],axis = 0)
-                unsorted = False
-        if unsorted:
-            return array
+# These are ports of my earlier algorithm project, but with extra lines inside to 
+# record each step of the array ordering process inside a matrix called "record"
+#################################
+# 1 // QUICKSORT
+#################################
 def quicksort_recorder(A):
     start = 0
     end = len(A)-1
@@ -92,15 +83,28 @@ def partition(A,start,end):
     if any(A != record[-1,:]):
         record = np.append(record,[A],axis = 0)
     return b-1
+#################################
+# 2 // BUBBLESORT
+#################################
+def bubblesort_recorder(array):
+    while True:
+        unsorted = True
+        for i in range(1,len(array)):
+            if array[i-1] > array[i]:
+                array[i-1],array[i] = array[i],array[i-1]
+                global record
+                record = np.append(record,[array],axis = 0)
+                unsorted = False
+        if unsorted:
+            return array
 ##################################################
     
 def drawframe(row):
-    #set up scale
+    #Draws a given list (not necessarily ordered) onto screen
     scale = Ny/max(row)
     barwidth = Nx/len(row)
     index = 0
     window.fill(bgcolor)
-    #DEBUG: Draw only 1st bar:
     for i in range(len(row)):
         if colorfade:
             currentbarcolor = (0,0,0)
@@ -113,24 +117,26 @@ def drawframe(row):
     return
     
 def play_animation(record):
-    for i in range(record.shape[0]): #for each row
+    #main animation loop process
+    for i in range(record.shape[0]): #for each row in "record" array
         surf.fill(bgcolor)
         drawframe(record[i,:]) 
         clock.tick(animFPS)
         window.blit(surf,(0,0))
+        #display.update() is used without arguments so that it also updates the FPS GUI element each frame
         pygame.display.update()
-        
         for event in pygame.event.get(): #detect events
             if event.type == pygame.QUIT or pygame.key.get_pressed()[27]: #detect attempted exit
                 pygame.quit()
                 sys.exit()
-            if pygame.key.get_pressed()[114]:
+            if pygame.key.get_pressed()[114]: #detect reset
                 menusetup()
                 return
     return
         
 def menusetup(N = defaultN, M = defaultM):
-    #generate array
+    #this function is called at the start of the program and on every "reset" call
+    #generate random unordered array
     array = []
     if uniformdist:
         for i in range(N):
@@ -151,25 +157,25 @@ def menusetup(N = defaultN, M = defaultM):
     pygame.display.update()
     return
 
+def main():
+    menusetup()
+    while True: #MAIN GAMELOOP
+        clock.tick(menuFPS)
+        for event in pygame.event.get(): #detect events
+            if event.type == pygame.QUIT or pygame.key.get_pressed()[27]: #detect attempted exit
+                pygame.quit()
+                sys.exit() 
+            if event.type == pygame.VIDEORESIZE: #detect resize
+                (Nx, Ny) = event.size
+                window = pygame.display.set_mode((Nx, Ny), pygame.RESIZABLE)
+                surf = pygame.display.set_mode((Nx, Ny), pygame.RESIZABLE)
+            if pygame.mouse.get_pressed()[0] or pygame.key.get_pressed()[32]: #play default anim on mouseclick or spacepress
+                play_animation(record)  
+            if pygame.key.get_pressed()[114]: #reset call
+                menusetup()
 
-menusetup()
-while True:
-    clock.tick(menuFPS)
-    for event in pygame.event.get(): #detect events
-        if event.type == pygame.QUIT or pygame.key.get_pressed()[27]: #detect attempted exit
-            pygame.quit()
-            sys.exit() 
-        if event.type == pygame.VIDEORESIZE: #detect resize
-            (Nx, Ny) = event.size
-            window = pygame.display.set_mode((Nx, Ny), pygame.RESIZABLE)
-            surf = pygame.display.set_mode((Nx, Ny), pygame.RESIZABLE)
-        if pygame.mouse.get_pressed()[0] or pygame.key.get_pressed()[32]:
-            play_animation(record)  
-        if pygame.key.get_pressed()[114]:
-            menusetup()
-            
-
-
+if __name__ == '__main__':
+    main()
         
         
         
